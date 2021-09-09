03aaad2c6e5edd6e373a3fdfc4bf3dc94aa49a1b
durvish2911 <72204035+Durvish2911@users.noreply.github.com>
2021-09-09 08:12:56 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+ var leftwrist=0;
+ var rightwrist=0;
+ var difference=0;
+ function setup(){
+     video=createCapture(VIDEO);
+     video.size(550,500);
+     canvas=createCanvas(550,550);
+     canvas.position(570,150);
+     pose=ml5.poseNet(video,modelloaded);
+     pose.on("pose",gotposes)
+ }
+ function modelloaded(){
+     console.log("model loaded");
+ }
+ function gotposes(results){
+  if(results.length>0){
+      console.log(results);
+      leftwrist=results[0].pose.leftWrist.x;
+      rightwrist=results[0].pose.rigthWrist.x;
+      difference=floor(leftwrist-rightwrist);
+  }
+ }
